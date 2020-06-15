0822423fe64681ce89af571026bc31e493b52eb7
coderqmo <66912703+coderqmo@users.noreply.github.com>
2020-06-15 18:37:02 +0300

    Add files via upload

---
+++ sqlmapsiteler.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import time 
+
+print("""
+Sqlnmap açılı sitelerin linkini Verir.
+            ########################
+            
+             ×_×    Coder QMO  ×_×
+             
+            ########################
+SQL Açıklı Siteler
+
+
+""")
+time.sleep(2)
+print("lütfen Bekleyiniz")
+time.sleep(3)
+print("")
+print("Siteler Hazirlaniyor...")
+time.sleep(2)
+print("")
+print("%50")
+print("")
+print("")
+time.sleep(1)
+print("")
+print("")
+time.sleep(1.5)
+print"%100"
+print("")
+print("")
+time.sleep(1.3)
+print"""Siteler : 
+http://www.epilogesmagazines.gr/viewarticle.php?productID=2757 """
+print("")
+time.sleep(2)
+print"""http://www.jetskiworld.gr/news_eng.php?id=627&offset=-20210&numrows=7&bg=""" 
+print("")
+time.sleep(2)
+print"""http://www.magenta.gr/index.php?option=com_alphacontent&section=3&ordering=12&limitstart=0&limit=10&Itemid=0"""
+print("")
+time.sleep(2)
+print"""http://www.energyproject.gr/english/project.php?id=10"""
+print("")
+time.sleep(2)
+print"""http://www.videographia.gr/eshop/addtobasket.php?id=3"""
+print("")
+time.sleep(2)
+print"""http://sunrentacar.harmonyresorts.gr/pages/car-details.php?lang=en&id=999999.9' """
+print("")
+time.sleep(2)
+print"""http://www.kosimmobilien.gr/product-it.php?id=55&prod=Ground%20Floor%20Appartement"""
+print("")
+time.sleep(2)
+print"""http://www.karellas.gr/productuk.php?id=-108+union+select+0"""
+print("")
+time.sleep(2)
+print"""http://www.akti.gr/accommodation.php?id=40"""
+print("")
+time.sleep(2)
+print"""http://www.vocha.gr/category.php?id="""
+print("")
+time.sleep(2)
+print"""http://www.bee-realestate.gr/property@.php?id=17375"""
+print("")
+time.sleep(2)
+print"""http://paideia-agrinio.gr/gr/main_d.php?id=&id1=1&id2=0"""
+print("")
+time.sleep(3.5)
+print("2. Linkler Hazırlanıyor...")
+time.sleep(1)
+print("")
+print("%50")
+print("")
+print("")
+print("")
+time.sleep(1.3)
+print("")
+print("")
+print("")
+print("%100")
+print("")
+print("")
+time.sleep(1.9)
+print""" 2.Linkler
+http://sunrentacar.harmonyresorts.gr/pages """
+print("")
+time.sleep(2)
+print"""http://www.real-value.gr/properties.php?id=38 """
+print("")
+time.sleep(2)
+print"""http://www.spep.gr/contacts/doodle.php?part= """
+print("")
+time.sleep(2)
+print"""http://www.autothessaly.gr/merchant_cars.php?id=&page=2 """
+print("")
+time.sleep(2)
+print"""http://www.ierapostolos.gr/news_article.php?ID= """
+print("")
+time.sleep(2)
+print"""http://www.pop3.gr/databases/demo1/index.php?Datain=4&header=$header&LID=2"""
+print("")
+time.sleep(2)
+print"""http://infoman.teikav.edu.gr/teachers/index.php?id="""
+print("")
+time.sleep(2)
+print"""http://www.dramagoal.gr/more.php?id=129"""
+print("")
+time.sleep(2)
+print"""http://www.kar.edu.gr/publications/xml/Pages.php?MagID=1&MagNo=3"""
+print("")
+time.sleep(2)
+print"""http://www.videographia.gr/eshop/addtobasket.php?id=3"""
+print("")
+time.sleep(2)
+print"""http://www.jockium.gr/link/index.php?show=owner&ID=47"""
+print("")
+time.sleep(2)
+print"""http://sunrentacar.harmonyresorts.gr/pages/offer-display.php?lang=en&id=999999.9"""
+print("")
+time.sleep(2)
+print"""http://www.officeconnection.gr/productsgr.php?id=6"""
+print("")
+time.sleep(2)
+print"""http://www.scott.gr/search.php?term=Complete&l=1"""
+print("")
+time.sleep(2)
+print"""http://www.spep.gr/contacts/doodle.php?part="""
+print("")
+time.sleep(2)
+print"""http://www.kosimmobilien.gr/product-it.php?id=55&prod=Ground%20Floor%20Appartement"""
+print("")
+time.sleep(2)
+print"""http://digitize.iema.gr/is_pi.php?phys_item_id=5826"""
+print("")
+time.sleep(2)
+print"""http://www.pop3.gr/databases/demo1/index.php?Datain=4&header=$header&LID=2"""
+print("")
+time.sleep(2)
+print"""http://www.jetskiworld.gr/news.php?id=626&offset=-39236&numrows=7&bg="""
+print("")
+time.sleep(2)
+print"""http://www.se-eida.gr/main.php?category=1"""
+print("")
+time.sleep(1.6)
+print("")
+print("")
+print("")
+print""" Site adresleri verildi
+"""
